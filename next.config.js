144bfbb2febcd46283977bb199d9bc680dc896dd
con2war <64367519+con2war@users.noreply.github.com>
2024-12-02 13:47:07 +0000

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone', // This ensures server-side features are enabled
 }
 
-module.exports = nextConfig  // Using CommonJS export instead of ES modules
+module.exports = nextConfig // Using CommonJS export instead of ES modules
